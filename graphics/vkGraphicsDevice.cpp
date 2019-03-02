8bdaa45aac08933fc55dc18a8891ff2727c87eba
OriginalDecode <originaldecode@outlook.com>
2019-03-02 18:21:51 +0100

    Working on the vulkan graphics device

--- graphics/vkGraphicsDevice.cpp
+++ graphics/vkGraphicsDevice.cpp
@@ -1,23 +1,21 @@
 #include "vkGraphicsDevice.h"
 
-#include <vulkan/vulkan.h>
 #include <cassert>
+#include <vulkan/vulkan.h>
 
 #include "Utilities.h"
 
 #include <vector>
 
-VkInstance pVulkanInstance = nullptr;
-
 VKAPI_ATTR VkBool32 VKAPI_CALL DebugReportCallback(
-    VkDebugReportFlagsEXT       /*flags*/,
-    VkDebugReportObjectTypeEXT  /*objectType*/,
-    uint64_t                    /*object*/,
-    size_t                      /*location*/,
-    int32_t                     /*messageCode*/,
-    const char*                 /*pLayerPrefix*/,
-    const char*                 /*pMessage*/,
-    void*                       /*pUserData*/)
+    VkDebugReportFlagsEXT /* flags */,
+    VkDebugReportObjectTypeEXT /* objectType */,
+    uint64_t /* object */,
+    size_t /* location */,
+    int32_t /* messageCode */,
+    const char* /* pLayerPrefix */,
+    const char* /* pMessage */,
+    void* /* pUserData */ )
 {
     // OutputDebugStringA(pMessage);
     // OutputDebugStringA("\n");
@@ -27,17 +25,16 @@ VKAPI_ATTR VkBool32 VKAPI_CALL DebugReportCallback(
 std::vector<const char*> GetDebugInstanceNames()
 {
     uint32 layer_count = 0;
-    vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
-    std::vector<VkLayerProperties> instance_layers;
-    instance_layers.reserve(layer_count);
+    vkEnumerateInstanceLayerProperties( &layer_count, nullptr );
+    std::vector<VkLayerProperties> instance_layers{ layer_count };
 
-    vkEnumerateInstanceLayerProperties(&layer_count, instance_layers.data());
-    
+    vkEnumerateInstanceLayerProperties( &layer_count, instance_layers.data() );
+
     std::vector<const char*> result;
-    for(const auto& layer : instance_layers)
+    for( const auto& layer : instance_layers )
     {
-        if( strcmp(layer.layerName, "VK_LAYER_LUNARG_standard_validation") == 0)
-            result.push_back("VK_LAYER_LUNARG_standard_validation");
+        if( strcmp( layer.layerName, "VK_LAYER_LUNARG_standard_validation" ) == 0 )
+            result.push_back( "VK_LAYER_LUNARG_standard_validation" );
     }
 
     return result;
@@ -45,57 +42,80 @@ std::vector<const char*> GetDebugInstanceNames()
 
 std::vector<const char*> GetDebugInstanceExtNames()
 {
-    
     uint32 extension_count = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+    vkEnumerateInstanceExtensionProperties( nullptr, &extension_count, nullptr );
+    std::vector<VkExtensionProperties> instance_extensions{ extension_count };
 
-    std::vector<VkExtensionProperties> instance_extensions;
-    instance_extensions.reserve(extension_count);
+    vkEnumerateInstanceExtensionProperties( nullptr, &extension_count, instance_extensions.data() );
 
-    vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, instance_extensions.data());
-
-    std::vector<const char*> result;
-    result.reserve(extension_count);
-
-    for(const auto& ext : instance_extensions)
+    std::vector<const char*> result{ extension_count };
+    for( const auto& ext : instance_extensions )
     {
-        if( strcmp( ext.extensionName, "VK_EXT_debug_report") == 0 )
-            result.push_back("VK_EXT_debug_report");
+        if( strcmp( ext.extensionName, "VK_EXT_debug_report" ) == 0 )
+            result.push_back( "VK_EXT_debug_report" );
     }
 
     return result;
 }
 
+std::vector<const char*> GetDebugDeviceLayerNames( VkPhysicalDevice device )
+{
+    uint32 layer_count = 0;
+    vkEnumerateDeviceLayerProperties( device, &layer_count, nullptr );
 
+    std::vector<VkLayerProperties> device_layers{ layer_count };
+    vkEnumerateDeviceLayerProperties( device, &layer_count, device_layers.data() );
+
+    std::vector<const char*> result;
+    for( const auto& p : device_layers )
+    {
+        if( strcmp( p.layerName, "VK_LAYER_LUNARG_standard_validation" ) == 0 )
+            result.push_back( "VK_LAYER_LUNARG_standard_validation" );
+    }
+
+    return result;
+}
 
 namespace Graphics
 {
     vkGraphicsDevice::vkGraphicsDevice()
     {
+
+        CreateInstance();
+        CreateDevice();
+    }
+
+    vkGraphicsDevice::~vkGraphicsDevice()
+    {
+        //
+    }
+
+    void vkGraphicsDevice::CreateInstance()
+    {
         VkApplicationInfo app_info = {
             VK_STRUCTURE_TYPE_APPLICATION_INFO, /* VkStructureType              sType */
             nullptr,                            /* const void*                  pNext */
             "Hello World!",                     /* const char*                  pApplicationName */
-            VK_MAKE_VERSION(1,0,0),             /* uint32_t                     applicationVersion */
+            VK_MAKE_VERSION( 1, 0, 0 ),         /* uint32_t                     applicationVersion */
             "Engine",                           /* const char*                  pEngineName */
-            VK_MAKE_VERSION(1,0,0),             /* uint32_t                     engineVersion */
+            VK_MAKE_VERSION( 1, 0, 0 ),         /* uint32_t                     engineVersion */
             VK_API_VERSION_1_1                  /* uint32_t                     apiVersion */
         };
-        
+
         std::vector<const char*> instance_extensions = {
-             "VK_KHR_surface", 
-             "VK_KHR_win32_surface"
+            "VK_KHR_surface",
+            "VK_KHR_win32_surface"
         };
 
         std::vector<const char*> instance_layers;
 #ifdef _DEBUG
         std::vector<const char*> debug_instance_layers = GetDebugInstanceNames();
-        instance_layers.insert(instance_layers.end(), debug_instance_layers.begin(), debug_instance_layers.end());
+        instance_layers.insert( instance_layers.end(), debug_instance_layers.begin(), debug_instance_layers.end() );
         // instance_layers.swap(debug_instance_layers);
 #endif
 
         VkInstanceCreateFlags create_flags = 0;
-        VkInstanceCreateInfo create_info {
+        VkInstanceCreateInfo create_info{
             VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, /* VkStructureType             sType; */
             nullptr,                                /* const void*                 pNext; */
             create_flags,                           /* VkInstanceCreateFlags       flags; */
@@ -106,22 +126,100 @@ namespace Graphics
             instance_extensions.data()              /* const char* const*          ppEnabledExtensionNames; */
         };
 
-        VkResult result = vkCreateInstance(&create_info, nullptr /*allocator*/, &pVulkanInstance);
-        assert(result == VK_SUCCESS && "Failed to create vulkan instance!");
-    }
-
-    vkGraphicsDevice::~vkGraphicsDevice()
-    {
-        //
+        VkResult result = vkCreateInstance( &create_info, nullptr /*allocator*/, &m_Instance );
+        assert( result == VK_SUCCESS && "Failed to create vulkan instance!" );
     }
 
     void vkGraphicsDevice::CreateDevice()
     {
         uint32 device_count = 0;
-        vkEnumeratePhysicalDevices(nullptr/*instance*/, &device_count, nullptr);
+        VkResult result = vkEnumeratePhysicalDevices( m_Instance, &device_count, nullptr );
+        assert( result == VK_SUCCESS && "Failed to enumerate devices" );
 
+        std::vector<VkPhysicalDevice> devices{ device_count };
+        result = vkEnumeratePhysicalDevices( m_Instance, &device_count, devices.data() );
+        assert( result == VK_SUCCESS && "Failed to enumerate devices" );
 
+        /*
+            * Find a physical device with a graphics queue
+        */
+        uint32_t output_graphics_device_index = 0;
+        for( auto device : devices )
+        {
+            uint32 property_count = 0;
+            vkGetPhysicalDeviceQueueFamilyProperties( device, &property_count, nullptr );
+
+            std::vector<VkQueueFamilyProperties> queue_properties{ property_count };
+            vkGetPhysicalDeviceQueueFamilyProperties( device, &property_count, queue_properties.data() );
+
+            for( size_t i = 0; i < queue_properties.size(); ++i )
+            {
+                const VkQueueFamilyProperties& property = queue_properties[i];
+                if( property.queueFlags & VK_QUEUE_GRAPHICS_BIT )
+                {
+                    if( !m_PhysicalDevice )
+                        m_PhysicalDevice = device;
+
+                    output_graphics_device_index = (uint32_t)i;
+                    return;
+                }
+            }
+        }
+
+        assert( m_PhysicalDevice );
+        VkDeviceQueueCreateFlags queue_flags = 0;
+
+        const float queue_priorities[] = { 1.f };
+
+        VkDeviceQueueCreateInfo device_queue_create_info = {
+            VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, /* VkStructureType                  sType; */
+            nullptr,                                    /* const void*                      pNext; */
+            queue_flags,                                /* VkDeviceQueueCreateFlags         flags; */
+            output_graphics_device_index,               /* uint32_t                         queueFamilyIndex; */
+            1,                                          /* uint32_t                         queueCount; */
+            queue_priorities                            /* const float*                     pQueuePriorities;  */
+        };
+
+        std::vector<const char*> device_layers;
+#ifdef _DEBUG
+        std::vector<const char*> device_debug_names = GetDebugDeviceLayerNames( m_PhysicalDevice );
+        device_layers.insert( device_layers.end(), device_debug_names.begin(), device_debug_names.end() );
+#endif
+
+        VkPhysicalDeviceFeatures enabled_features;
+        memset( &enabled_features, 0, sizeof( enabled_features ) );
+        enabled_features.shaderClipDistance = true;
+
+        std::vector<const char*> device_extensions = {
+            "VK_KHR_swapchain"
+        };
+
+        VkDeviceCreateFlags device_create_flags = 0;
+        VkDeviceCreateInfo device_create_info = {
+            VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO, /* VkStructureType                    sType; */
+            nullptr,                              /* const void*                        pNext; */
+            device_create_flags,                  /* VkDeviceCreateFlags                flags; */
+            1,                                    /* uint32_t                           queueCreateInfoCount; */
+            &device_queue_create_info,            /* const VkDeviceQueueCreateInfo*     pQueueCreateInfos; */
+            (uint32_t)device_layers.size(),       /* uint32_t                           enabledLayerCount; */
+            device_layers.data(),                 /* const char* const*                 ppEnabledLayerNames; */
+            (uint32_t)device_extensions.size(),   /* uint32_t                           enabledExtensionCount; */
+            device_extensions.data(),             /* const char* const*                 ppEnabledExtensionNames; */
+            &enabled_features                     /* const VkPhysicalDeviceFeatures*    pEnabledFeatures;         */
+        };
+
+        result = vkCreateDevice( m_PhysicalDevice, &device_create_info, nullptr, &m_Device );
+        assert( result != VK_SUCCESS && "Failed to create vulkan device!" );
+
+        vkGetDeviceQueue( m_Device, output_graphics_device_index, 0, &m_Queue );
+        assert( result != VK_SUCCESS && "Failed to create vulkan queue!" );
     }
+
+    void vkGraphicsDevice::CreateSwapchain()
+    {
+        
+    }
+
 
 
 }; //namespace Graphics
--- graphics/vkGraphicsDevice.h
+++ graphics/vkGraphicsDevice.h
@@ -1,4 +1,10 @@
 #pragma once
+#include "Core/utilities/utilities.h"
+
+LPE_DEFINE_HANDLE( VkInstance );
+LPE_DEFINE_HANDLE( VkDevice );
+LPE_DEFINE_HANDLE( VkPhysicalDevice );
+LPE_DEFINE_HANDLE( VkQueue );
 
 namespace Graphics
 {
@@ -7,10 +13,16 @@ namespace Graphics
     public:
         vkGraphicsDevice();
         ~vkGraphicsDevice();
+
     private:
+        void CreateInstance();
         void CreateDevice();
+        void CreateSwapchain();
 
-
-
+        VkQueue m_Queue = nullptr;
+        VkDevice m_Device = nullptr;
+        VkInstance m_Instance = nullptr;
+        VkPhysicalDevice m_PhysicalDevice = nullptr;
     };
+
 }; // namespace Graphics
