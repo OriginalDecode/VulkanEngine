79f42fe51c80d2b8be58c553292d9ec4d1ca8dc6
OriginalDecode <originaldecode@outlook.com>
2019-03-02 18:20:26 +0100

    Implemented the Murmur3 hash and a printlasterror function

--- core/utilities/utilities.h
+++ core/utilities/utilities.h
@@ -1,9 +1,16 @@
 #pragma once
+#include "Core/Types.h"
 #include <string>
+
+#define LPE_DEFINE_HANDLE( object ) typedef struct object##_T* object;
 namespace Core
 {
-    std::wstring ToWideString(const std::string& str);
-    std::string ToString(const std::wstring& str);
+    std::wstring ToWideString( const std::string& str );
+    std::string ToString( const std::wstring& str );
 
+    uint64 Hash( const std::string& str );
+    uint64 Hash( const std::string& str, uint32 seed );
 
+    void DebugPrintLastError();
+    
 }; //namespace Core
