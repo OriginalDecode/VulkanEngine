85e789f94c60a4f87458e45881fe37b11ed277f7
OriginalDecode <originaldecode@outlook.com>
2019-04-23 09:37:52 +0200

    added suffix to shaders. Might change this in the future to just reflect shadertype instead of spvX

--- compileShaders.py
+++ compileShaders.py
@@ -27,14 +27,33 @@ def main():
     print("output : " + outputFolder)
     shaderFiles = list()
 
+    fileTypes = { 
+        ".vert" : "v", 
+        ".frag" : "f", 
+        ".comp" : "c", 
+        ".geom" : "g", 
+        ".tese" : "te", 
+        ".tesc" : "tc" 
+        }
+
     appendToList(shaderFiles, folder, "/*.vert")
+    appendToList(shaderFiles, folder, "/*.frag")
+    appendToList(shaderFiles, folder, "/*.comp")
+    appendToList(shaderFiles, folder, "/*.geom")
+    appendToList(shaderFiles, folder, "/*.tese")
+    appendToList(shaderFiles, folder, "/*.tesc")
+    
     for file in shaderFiles:
+        suffix = "unknown"
+        for key, value in fileTypes.iteritems():
+            if file.find(key) != -1:
+                suffix = value
 
         outputFile = file[file.rfind('\\')+1:]
         print(outputFile)
         command = "%VKBIN%/glslangValidator.exe " #validator path
         command += "-e main " #entrypoint, this should probably be changed into something else ?
-        command += "-o " + outputFolder + "/" + outputFile[:-4] + "spv " #output
+        command += "-o " + outputFolder + "/" + outputFile[:-4] + "spv" + suffix + " " #output
         command += "-V -D " #-V needed to generate binary, -D to tell the validator that it's HLSL code.
         command += file 
         os.system(command) 
