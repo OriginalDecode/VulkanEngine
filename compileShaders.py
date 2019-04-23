55795e2e044b5987f05f9e0165909080037328c1
OriginalDecode <originaldecode@outlook.com>
2019-04-23 12:58:18 +0200

    updated compile script to output with the file ext that was inputted

--- compileShaders.py
+++ compileShaders.py
@@ -27,33 +27,24 @@ def main():
     print("output : " + outputFolder)
     shaderFiles = list()
 
-    fileTypes = { 
-        ".vert" : "v", 
-        ".frag" : "f", 
-        ".comp" : "c", 
-        ".geom" : "g", 
-        ".tese" : "te", 
-        ".tesc" : "tc" 
-        }
+    fileTypes = [ 
+        "*.vert", 
+        "*.frag", 
+        "*.comp", 
+        "*.geom", 
+        "*.tese", 
+        "*.tesc" 
+    ]
 
-    appendToList(shaderFiles, folder, "/*.vert")
-    appendToList(shaderFiles, folder, "/*.frag")
-    appendToList(shaderFiles, folder, "/*.comp")
-    appendToList(shaderFiles, folder, "/*.geom")
-    appendToList(shaderFiles, folder, "/*.tese")
-    appendToList(shaderFiles, folder, "/*.tesc")
+    for fileType in fileTypes :
+        appendToList(shaderFiles, folder, "/"+fileType)
     
     for file in shaderFiles:
-        suffix = "unknown"
-        for key, value in fileTypes.iteritems():
-            if file.find(key) != -1:
-                suffix = value
-
         outputFile = file[file.rfind('\\')+1:]
         print(outputFile)
         command = "%VKBIN%/glslangValidator.exe " #validator path
         command += "-e main " #entrypoint, this should probably be changed into something else ?
-        command += "-o " + outputFolder + "/" + outputFile[:-4] + "spv" + suffix + " " #output
+        command += "-o " + outputFolder + "/" + outputFile + " " #output
         command += "-V -D " #-V needed to generate binary, -D to tell the validator that it's HLSL code.
         command += file 
         os.system(command) 
