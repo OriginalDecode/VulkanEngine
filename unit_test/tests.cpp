f16f7fc0d7523fe980dd3a5746631cb879945255
OriginalDecode <originaldecode@outlook.com>
2019-05-08 15:35:23 +0200

    adding some comments about different macros in gtests

--- unit_test/tests.cpp
+++ unit_test/tests.cpp
@@ -1,9 +1,27 @@
 #include <cstdio>
 #include "gtest/gtest.h"
 
+/*
+	different macros for unit tests
+
+	EXPECT_EQ // equal
+	EXPECT_NE // not equal
+	EXPECT_LT // less than
+	EXPECT_LE // lesser or equal than
+	EXPECT_GT // greater than
+	EXPECT_GE // greater or equal than
+
+	Expect does not assume fatal
+	
+	ASSERT_TRUE //fatal
+	ASSERT_FALSE //fatal
+
+
+*/
+
 TEST( OverarchingTest, Testname )
 {
-	EXPECT_EQ( 1, 1 );
+	ASSERT_EQ( 1, 0 );
 }
 
 GTEST_API_ int main( int argc, char** argv )
