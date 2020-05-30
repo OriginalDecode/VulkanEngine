294515efe9024b2987afe5d3a22009056df1b98a
OriginalDecode <originaldecode@outlook.com>
2020-05-31 01:11:37 +0200

    Implemented AllocateMemory and CreateBuffer in the Device class

--- source/graphics/VlkDevice.cpp
+++ source/graphics/VlkDevice.cpp
@@ -1,6 +1,8 @@
 #include "VlkDevice.h"
 #include "VlkPhysicalDevice.h"
 
+#include "logger/Debug.h"
+
 #include <Windows.h>
 #include <vulkan/vulkan_core.h>
 #include <cassert>
@@ -10,34 +12,71 @@ namespace Graphics
 	const char* debugLayers[] = { "VK_LAYER_LUNARG_standard_validation" };
 	const char* deviceExt[] = { "VK_KHR_swapchain" };
 
-	VlkDevice::~VlkDevice() { Release( nullptr ); }
+	VlkDevice::~VlkDevice()
+	{
+		Release(nullptr);
+	}
 
-	void VlkDevice::Release( IGfxDevice* ) { vkDestroyDevice( m_Device, nullptr ); }
+	void VlkDevice::Release(IGfxDevice*)
+	{
+		vkDestroyDevice(m_Device, nullptr);
+	}
 
-	VkSwapchainKHR VlkDevice::CreateSwapchain( const VkSwapchainCreateInfoKHR& createInfo ) const
+	VkSwapchainKHR VlkDevice::CreateSwapchain(const VkSwapchainCreateInfoKHR& createInfo) const
 	{
 		VkSwapchainKHR swapchain = nullptr;
-		VkResult result = vkCreateSwapchainKHR( m_Device, &createInfo, nullptr, &swapchain );
-		assert( result == VK_SUCCESS && "Failed to create swapchain" );
+		VkResult result = vkCreateSwapchainKHR(m_Device, &createInfo, nullptr, &swapchain);
+		assert(result == VK_SUCCESS && "Failed to create swapchain");
 		return swapchain;
 	}
 
-	void VlkDevice::DestroySwapchain( VkSwapchainKHR pSwapchain )
+	void VlkDevice::DestroySwapchain(VkSwapchainKHR pSwapchain)
 	{
-		vkDestroySwapchainKHR( m_Device, pSwapchain, nullptr );
+		vkDestroySwapchainKHR(m_Device, pSwapchain, nullptr);
 	}
 
-	void VlkDevice::GetSwapchainImages( VkSwapchainKHR* pSwapchain, std::vector<VkImage>* scImages )
+	void VlkDevice::GetSwapchainImages(VkSwapchainKHR* pSwapchain, std::vector<VkImage>* scImages)
 	{
 		uint32 imageCount = 0;
-		VkResult result = vkGetSwapchainImagesKHR( m_Device, *pSwapchain, &imageCount, nullptr );
-		assert( result == VK_SUCCESS && "failed to get imagecount" );
-		scImages->resize( imageCount );
-		result = vkGetSwapchainImagesKHR( m_Device, *pSwapchain, &imageCount, scImages->data() );
-		assert( result == VK_SUCCESS && "failed to get swapchainimages" );
+		VkResult result = vkGetSwapchainImagesKHR(m_Device, *pSwapchain, &imageCount, nullptr);
+		assert(result == VK_SUCCESS && "failed to get imagecount");
+		scImages->resize(imageCount);
+		result = vkGetSwapchainImagesKHR(m_Device, *pSwapchain, &imageCount, scImages->data());
+		assert(result == VK_SUCCESS && "failed to get swapchainimages");
 	}
 
-	void VlkDevice::Init( VlkPhysicalDevice* physicalDevice )
+	VkDeviceMemory VlkDevice::AllocateMemory(const VkMemoryRequirements& requirements, VlkPhysicalDevice* physDevice)
+	{
+		VkDeviceMemory memory;
+		VkMemoryAllocateInfo allocInfo = {};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = requirements.size;
+		allocInfo.memoryTypeIndex = physDevice->FindMemoryType(requirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+		if(vkAllocateMemory(m_Device, &allocInfo, nullptr, &memory) != VK_SUCCESS)
+			ASSERT(false, "Failed to allocate memory on GPU!");
+
+		return memory;
+	}
+
+	VkBuffer VlkDevice::CreateBuffer(const VkBufferCreateInfo& createInfo, VkDeviceMemory* memory, VlkPhysicalDevice* physDevice)
+	{
+		VkBuffer buffer = nullptr;
+		if(vkCreateBuffer(m_Device, &createInfo, nullptr, &buffer) != VK_SUCCESS)
+			ASSERT(false, "Failed to create vertex buffer!");
+
+		VkMemoryRequirements memRequirements;
+		vkGetBufferMemoryRequirements(m_Device, buffer, &memRequirements);
+
+		*memory = AllocateMemory(memRequirements, physDevice);
+
+		if(vkBindBufferMemory(m_Device, buffer, *memory, 0) != VK_SUCCESS)
+			ASSERT(false, "Failed to bind buffer memory!");
+
+		return buffer;
+	}
+
+	void VlkDevice::Init(VlkPhysicalDevice* physicalDevice)
 	{
 		// queue create info
 		const float queue_priorities[] = { 1.f };
@@ -57,15 +96,15 @@ namespace Graphics
 		createInfo.queueCreateInfoCount = 1;
 		createInfo.pQueueCreateInfos = &queueCreateInfo;
 #ifdef _DEBUG
-		createInfo.enabledLayerCount = ARRSIZE( debugLayers );
+		createInfo.enabledLayerCount = ARRSIZE(debugLayers);
 		createInfo.ppEnabledLayerNames = debugLayers;
 #endif
-		createInfo.enabledExtensionCount = ARRSIZE( deviceExt );
+		createInfo.enabledExtensionCount = ARRSIZE(deviceExt);
 		createInfo.ppEnabledExtensionNames = deviceExt;
 		createInfo.pEnabledFeatures = &enabled_features;
 
-		m_Device = physicalDevice->CreateDevice( createInfo );
+		m_Device = physicalDevice->CreateDevice(createInfo);
 
-		vkGetDeviceQueue( m_Device, physicalDevice->GetQueueFamilyIndex(), 0, &m_Queue );
+		vkGetDeviceQueue(m_Device, physicalDevice->GetQueueFamilyIndex(), 0, &m_Queue);
 	}
 }; // namespace Graphics
--- source/graphics/VlkDevice.h
+++ source/graphics/VlkDevice.h
@@ -21,20 +21,29 @@ namespace Graphics
 		VlkDevice() = default;
 		~VlkDevice();
 
-		void Init( VlkPhysicalDevice* physicalDevice );
+		void Init(VlkPhysicalDevice* physicalDevice);
 
-		VkDevice GetDevice() const { return m_Device; }
-		VkQueue GetQueue() const { return m_Queue; }
+		VkDevice GetDevice() const
+		{
+			return m_Device;
+		}
+		VkQueue GetQueue() const
+		{
+			return m_Queue;
+		}
 
-		VkSwapchainKHR CreateSwapchain( const VkSwapchainCreateInfoKHR& createInfo ) const;
-		void DestroySwapchain( VkSwapchainKHR pSwapchain );
+		VkSwapchainKHR CreateSwapchain(const VkSwapchainCreateInfoKHR& createInfo) const;
+		void DestroySwapchain(VkSwapchainKHR pSwapchain);
 
-		void GetSwapchainImages( VkSwapchainKHR* pSwapchain, std::vector<VkImage>* scImages );
+		void GetSwapchainImages(VkSwapchainKHR* pSwapchain, std::vector<VkImage>* scImages);
+
+		VkDeviceMemory AllocateMemory(const VkMemoryRequirements& requirements, VlkPhysicalDevice* physDevice);
+		VkBuffer CreateBuffer(const VkBufferCreateInfo& createInfo, VkDeviceMemory* memory, VlkPhysicalDevice* physDevice);
 
 	private:
-		void Release( IGfxDevice* device ) override;
+		void Release(IGfxDevice* device) override;
 		VkDevice m_Device = nullptr;
 		VkQueue m_Queue = nullptr;
 	};
 
-}; //namespace Graphics
+}; // namespace Graphics
